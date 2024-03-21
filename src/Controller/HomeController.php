20403df07f03af0844530116afeacfb0b8b27d50
Lurius-Kitsune <lucasfox@outlook.fr>
2024-03-21 11:34:11 +0100

    🔧 Added new organisation in visual

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MenuController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'acceuil')]
     public function index(): Response
     {
-        return $this->render('menu/index.html.twig', [
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'MenuController',
         ]);
     }
