450da5f4f3c09afdf5fcbe545cc506109eacd2c8
Lurius-Kitsune <lucasfox@outlook.fr>
2024-03-31 15:05:15 +0200

    🔧Ajout de la creation d'un compte     -> Envoie d'un mail de verification     -> Verification du numero de licence

--- src/Entity/Compte.php
+++ src/Entity/Compte.php
@@ -6,10 +6,12 @@ use App\Repository\CompteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: CompteRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Compte implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -34,6 +36,9 @@ class Compte implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\OneToMany(targetEntity: Inscription::class, mappedBy: 'compte')]
     private Collection $inscription;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -167,6 +172,18 @@ class Compte implements UserInterface, PasswordAuthenticatedUserInterface
                 $inscription->setCompte(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
