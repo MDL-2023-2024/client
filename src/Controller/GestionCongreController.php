04175c382d3f2d23dda8bafaeed8adfce3401672
Lurius-Kitsune <lucasfox@outlook.fr>
2024-04-02 14:15:07 +0200

    🔧Added vacation Form

--- src/Controller/GestionCongreController.php
+++ src/Controller/GestionCongreController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Atelier;
 use App\Entity\Theme;
+use App\Entity\Vacation;
 use App\Form\AteliersFormType;
 use App\Form\ThemeFormType;
+use App\Form\VacationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,15 +21,20 @@ class GestionCongreController extends AbstractController
     {
         $atelier = new Atelier();
         $theme = new Theme();
+        $vacation = new Vacation();
 
         $formAtelier = $this->createForm(AteliersFormType::class, $atelier);
         $formAtelier->handleRequest($request);
 
         $formTheme = $this->createForm(ThemeFormType::class, $theme);
         $formTheme->handleRequest($request);
+
+        $formVacation = $this->createForm(VacationFormType::class, $vacation);
+        $formVacation->handleRequest($request);
         return $this->render('gestion_congre/index.html.twig', [
             'atelierForm' => $formAtelier->createView(),
             'themeForm' => $formTheme->createView(),
+            'vacationForm' => $formVacation->createView(),
         ]);
     }
 }
