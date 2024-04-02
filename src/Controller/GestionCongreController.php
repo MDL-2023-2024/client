a56b3003d14d0ebbcf810158754a750601a3f88f
Lurius-Kitsune <lucasfox@outlook.fr>
2024-04-02 14:58:41 +0200

    🔧 Information sur la non préscence d'atelier + Insertion d'un atelier

--- src/Controller/GestionCongreController.php
+++ src/Controller/GestionCongreController.php
@@ -9,6 +9,7 @@ use App\Form\AteliersFormType;
 use App\Form\ThemeFormType;
 use App\Form\VacationFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\PseudoTypes\False_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,21 +21,34 @@ class GestionCongreController extends AbstractController
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $atelier = new Atelier();
-        $theme = new Theme();
-        $vacation = new Vacation();
+        $atelierList = $entityManager->getRepository(Atelier::class)->findAll();
+        
+        if ($atelierList != null) {
+            $theme = new Theme();
+            $vacation = new Vacation();
 
+            $formTheme = $this->createForm(ThemeFormType::class, $theme);
+            $formTheme = $formTheme->handleRequest($request)->createView();
+
+            $formVacation = $this->createForm(VacationFormType::class, $vacation);
+            $formVacation = $formVacation->handleRequest($request)->createView();
+        } else {
+            $formTheme = null;
+            $formVacation = null;
+        }
         $formAtelier = $this->createForm(AteliersFormType::class, $atelier);
         $formAtelier->handleRequest($request);
 
-        $formTheme = $this->createForm(ThemeFormType::class, $theme);
-        $formTheme->handleRequest($request);
-
-        $formVacation = $this->createForm(VacationFormType::class, $vacation);
-        $formVacation->handleRequest($request);
+        if ($formAtelier->isSubmitted() && $formAtelier->isValid()) {
+            $entityManager->persist($atelier);
+            $entityManager->flush();
+            $this->addFlash('success', 'Atelier ajouté avec succès !');
+            return $this->redirectToRoute('gestion_congre');
+        }
         return $this->render('gestion_congre/index.html.twig', [
             'atelierForm' => $formAtelier->createView(),
-            'themeForm' => $formTheme->createView(),
-            'vacationForm' => $formVacation->createView(),
+            'themeForm' => $formTheme,
+            'vacationForm' => $formVacation,
         ]);
     }
 }
