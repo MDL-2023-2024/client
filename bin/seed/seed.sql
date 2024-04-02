4899b76975a8e5f57b761a870664fca7f2f3dc2f
Lurius-Kitsune <LucasFox@outlook.fr>
2024-04-02 09:23:42 +0200

    🔧Ajout de la creation d'utilisateur

--- bin/seed/seed.sql
+++ bin/seed/seed.sql
@@ -52,3 +52,11 @@ INSERT INTO atelier_theme (atelier_id, theme_id) VALUES
 (1, 2), -- Lier 'Atelier Découverte' à 'Escrime Moderne'
 (2, 1), -- Lier 'Atelier Avancé' à 'Escrime Classique'
 (2, 2); -- Lier 'Atelier Avancé' à 'Escrime Moderne'
+
+LOCK TABLES `compte` WRITE;
+/*!40000 ALTER TABLE `compte` DISABLE KEYS */;
+INSERT INTO `compte` VALUES (1,'lucasfox@outlook.fr','[\"ROLE_ADMIN\"]','$2y$13$E2DGrmfbFj4GDbu12pGlzuVxgbpgiIL5kjTPZOGQR.SR3uhH5nQiy','16360514319',1);
+INSERT INTO `compte` VALUES (1,'dembelemathis@gmail.fr','[\"ROLE_ADMIN\"]','$2y$13$E2DGrmfbFj4GDbu12pGlzuVxgbpgiIL5kjTPZOGQR.SR3uhH5nQiy','1636',1);
+/*!40000 ALTER TABLE `compte` ENABLE KEYS */;
+-- mdp btssio2024
+UNLOCK TABLES;
