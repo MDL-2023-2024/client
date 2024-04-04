098b0c85d7b8fabdb39103c9439e6891b0a03830
Lurius-Kitsune <lucasfox@outlook.fr>
2024-04-04 17:28:59 +0200

    🔧 Tableau d'insertion thême / atelier

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Hotel;
+use App\Entity\Theme;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,8 +15,10 @@ class HomeController extends AbstractController
     public function index(ManagerRegistry $doctrine): Response
     {
         $hotels = $doctrine->getRepository(Hotel::class)->findAll();
+        $themes = $doctrine->getRepository(Theme::class)->findAll();
         return $this->render('home/index.html.twig', [
             'hotels' => $hotels,
+            'themes' => $themes,
         ]);
     }
 }
